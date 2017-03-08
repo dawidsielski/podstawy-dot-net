17124018ca0fc28574d212fa46456f47362907d4
turbodivx <dawid.sielski@outlook.com>
2017-03-08 23:58:29 +0100

    Classes and Interfaces done

--- Interfaces/Form1.cs
+++ Interfaces/Form1.cs
@@ -12,6 +12,36 @@ namespace Interfaces
 {
     public partial class Form1 : Form
     {
+        interface IAccelerate
+        {
+            int Accelerate(int change);
+        }
+        interface IDive
+        {
+            int Dive(int change);
+        }
+        interface IRise
+        {
+            int Rise(int change);
+        }
+
+        class Car
+        {
+
+        }
+        class Submarine
+        {
+
+        }
+        class Plane
+        {
+
+        }
+        class Computer
+        {
+
+        }
+
         public Form1()
         {
             InitializeComponent();
