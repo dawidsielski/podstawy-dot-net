4bdae3d9329e5242c497f8de62892432aafef738
Dawid Sielski <dawid.sielski@outlook.com>
2017-03-13 23:45:06 +0100

    First and Second sub ex done.

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -18,5 +18,67 @@ namespace Interfaces
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+    }
+
+    public class Car : IAccelerate, IRise
+    {
+        int acceleration;
+        int rise;
+        public void Accelerate(int change)
+        {
+            acceleration = change;
+        }
+        public void Rise(int change)
+        {
+            rise = change;
+        }
+    }
+    public class Submarine : IDive, IAccelerate
+    {
+        int dive;
+        int accelerate;
+        public void Dive( int change)
+        {
+            dive = change;
+        }
+        public void Accelerate(int change)
+        {
+            accelerate = change;
+        }
+    }
+    public class Plane: IRise, IDive
+    {
+        int rise;
+        int dive;
+        public void Rise(int change)
+        {
+            rise = change;
+        }
+        public void Dive(int change)
+        {
+            dive = change;
+        }
+    }
+    public class Computer
+    {
+
+    }
+
+
+
+    public interface IAccelerate
+    {
+        void Accelerate(int change);
+    }
+
+    public interface IDive
+    {
+        void Dive(int change);
+    }
+
+    public interface IRise
+    {
+        void Rise(int change);
     }
 }
